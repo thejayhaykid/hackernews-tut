56806d76198cd20cde20b24f194a801e6e539c62
Jake <jayhay28324@gmail.com>
2019-12-31 19:16:32 -0500

    35:58 point in tutorial

--- src/App.js
+++ src/App.js
@@ -1,3 +1,14 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import { getStoryIds } from './services/hnApi';
 
-export const App() => <p>Hello</p>
+export const App = () => {
+  const [storyIds, setStoryIds] = useState([]);
+
+  useEffect(() => {
+    getStoryIds().then(data => setStoryIds(data));
+  }, []);
+
+  return (
+    <p>{JSON.stringify(storyIds)}</p>
+  )
+}
