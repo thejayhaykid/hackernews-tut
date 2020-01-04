c32092ae9329a5cd4e4bb2857a85d23f9553ae46
Jake <jayhay28324@gmail.com>
2020-01-04 10:25:30 -0500

    55:56 in tut

--- src/components/story.js
+++ src/components/story.js
@@ -3,9 +3,15 @@ import { getStory } from '../services/hnApi';
 
 export const Story = ({ storyId }) => {
     const [story, setStory] = useState({});
+
     useEffect(() => {
         getStory(storyId).then(data => data && data.url && setStory(data));
     }, []);
 
-    return <p>I am a story! {storyId}</p>;
+    return story && story.url ? (
+        <div>
+            <a href={story.url}><p>{story.title}</p></a>
+            <p>By: {story.by} -- Posted: {story.time}</p>
+        </div>
+    ) : null;
 }
